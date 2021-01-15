c726377ae5e95d453a95b997e9b28f39fb875c18
hulya <hulyakarakayaa06@gmail.com>
2021-01-15 13:16:53 -0800

    add paths for post and get requests

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,23 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res) => {
+  res.render('index');
 });
+
+router.post('/', (req, res) => {
+  const {
+    source,
+    attributed,
+    quote
+  } = req.body;
+  res.render('index', {
+    source,
+    attributed,
+    quote
+  });
+});
+
+
 
 module.exports = router;
--- test/features/user-visits-root-test.js
+++ test/features/user-visits-root-test.js
@@ -1,22 +1,25 @@
+// test/features/user-visits-root-test.js
 const { assert } = require("chai");
 
 describe("User visits root", () => {
   describe("posting a quote", () => {
-    it("saves quote and metadata submitted by user", () => {
+    it("saves quotes and meta data submitted by the user", () => {
+      // Setup
       const quote =
-        "Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure";
+        "Our deepest fear is not that we are inadequate. Our deepest fear is that we are powerful beyond measure.";
       const attributed = "Marianne Williamson";
       const source =
-        "A Return to Love: Reflections on the Principles of A Course in Miracles.";
+        "A Return to Love: Reflections on the Principles of A Course in Miracles";
+      // Exercise
       browser.url("/");
-      browser.setValue("textarea[id=quote]", quote);
-      browser.setValue("input[id=attributed]", attributed);
-      browser.setValue("input[id=source]", source);
+      browser.setValue("#quote", quote);
+      browser.setValue("#attributed", attributed);
+      browser.setValue("#source", source);
       browser.click("input[type=submit]");
-
-      assert.include(browser.getText("#quote"), quote);
-      assert.include(browser.getText("#attributed"), attributed);
-      assert.include(browser.getText("#source"), source);
+      // Verify
+      assert.include(browser.getText("#quotes"), quote);
+      assert.include(browser.getText("#quotes"), attributed);
+      assert.include(browser.getText("#quotes"), source);
     });
   });
 });
